03193e51e31b3301729d31541093a89e72618cf9
Andrés Segura Sáez <andres14.ass@gmail.com>
2026-01-27 20:30:53 +0100

    refact(cars.controller): refact imports create and update dto

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -9,8 +9,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
-import { CreateCarDto } from './dto/create-car.dto';
-import { UpdateCarDto } from './dto/update-car.dto';
+import { CreateCarDto, UpdateCarDto } from './dto';
 
 @Controller('cars')
 export class CarsController {
